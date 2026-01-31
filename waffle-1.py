850352d1ce082a4c53397c24ecbfa5b687b1669e
screaming in agony <ameliar51@nycstudents.net>
2026-01-30 19:18:44 -0500

    wd-1

--- waffle-1.py
+++ waffle-1.py
@@ -1,7 +1,46 @@
-""" import turtle
-from turtle import *
-t = Turtle() """
 
-x = 3
+""" x = 3
 y = float(3)
-print (x,y)
+print (x,y) """
+
+""" x = "this is a thing"
+y = x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+""" values = [1, 2.23, 5, 7, 2, 30, 15]
+print(values)
+for i in values:
+    print(i)
+
+print(values[0])
+print(values[6]) """
+
+""" x = input
+y = x.split( )
+print(y)
+ """
+""" day_of_week = input("what day is it?")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect") """
+
+""" x = input()
+y = x.split()
+print(y) """
+""" 
+x = "test"
+print(f"hello {x}")
+
+temp = 68
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold') """
+
+num = input()
+if num 
