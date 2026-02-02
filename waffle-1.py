1be0c3588ea4f7e33eed1af69e3572cee65116b2
screaming in agony <ameliar51@nycstudents.net>
2026-02-02 10:16:55 -0500

    day-4

--- waffle-1.py
+++ waffle-1.py
@@ -9,6 +9,12 @@ z = y[0]
 print(y)
 print(z) """
 
+x = input("Enter a sentence")
+y = x.split()
+z = 
+print(y)
+print(z)
+
 """ values = [1, 2.23, 5, 7, 2, 30, 15]
 print(values)
 for i in values:
@@ -42,5 +48,20 @@ elif temp == 68:
 else:
     print('cold') """
 
-num = input()
-if num 
+""" x = int(input("enter a number"))
+if (x % 2) == 0:
+    print("even")
+else:
+    print("odd") """
+
+""" x = input("Was the service bad, okay, good, or great?")
+if x == "bad":
+    print("0%")
+if x == "okay":
+    print("15%")
+if x == "good":
+    print("20%")
+if x == "great":
+    print("25%") """
+
+""" num = int(input("enter a number")) """
